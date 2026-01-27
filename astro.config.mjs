f9b058644ffc2dea02f7dfa68fc6aad5a626f7f3
Maximilien Richald <maximilien.richald@outlook.com>
2026-01-27 18:15:28 +0100

    add github page deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://masseub.github.io',
+  base: '/maxreactengineering',
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
